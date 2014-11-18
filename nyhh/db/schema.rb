a85fe15576d96a4aa7ec496d3c0ad50c28160cf8
RJ Dabbar <rjdabbar@gmail.com>
2014-11-18 17:16:51 -0500

    add flyer to event model

--- nyhh/db/schema.rb
+++ nyhh/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141114220535) do
+ActiveRecord::Schema.define(version: 20141118221501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,10 @@ ActiveRecord::Schema.define(version: 20141114220535) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "approved"
+    t.string   "flyer_file_name"
+    t.string   "flyer_content_type"
+    t.integer  "flyer_file_size"
+    t.datetime "flyer_updated_at"
   end
 
   create_table "users", force: true do |t|
