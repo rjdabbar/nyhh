a1f0d2b2d365f60391dc86e3c1a476fe240b92b3
eganpg <eganpg@gmail.com>
2014-11-24 12:04:40 -0800

    adding styling to show events page

--- nyhh/db/schema.rb
+++ nyhh/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141124181901) do
+ActiveRecord::Schema.define(version: 20141124194734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,11 @@ ActiveRecord::Schema.define(version: 20141124181901) do
     t.string   "flyer_content_type"
     t.integer  "flyer_file_size"
     t.datetime "flyer_updated_at"
+    t.string   "address"
+    t.string   "street"
+    t.string   "city"
+    t.string   "state"
+    t.integer  "zip"
   end
 
   create_table "users", force: true do |t|
