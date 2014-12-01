49f63b45bf450401dd8f808afc8712cfc9a74fe4
eganpg <eganpg@gmail.com>
2014-12-01 15:44:36 -0800

    added approval in the edit admin

--- nyhh/app/admin/event.rb
+++ nyhh/app/admin/event.rb
@@ -1,47 +1,54 @@
 ActiveAdmin.register Event do
-  form do |f|
-    f.input :event_name
-    f.input :location
-    f.input :borough
-    f.input :ticket_price
-    f.select :approved, options_for_select([["Select One", ""], "Pending", "Accepted", "Rejected"]), :class => 'genForm_dropBox'
-    f.input :ticket_add_1
-    f.input :ticket_add_2
-    f.input :featured
-    f.input :user_id
-    f.input :date
-    f.input :avatar
-    f.input :flyer
-    f.input :description
-    f.input :latitude
-    f.input :longitude
-    f.input :venue
-    f.input :url
-    f.input :start
-    f.input :end
-    f.submit 'submit'
-  end
+    
+    form do |f|
+      f.select :approved, options_for_select([["Select One", ""], "Cell", "Work", "Office", "Home", "Other"]), :class => 'genForm_dropBox'
+      f.input :event_name 
+      f.input :location 
+      f.input :borough
+      f.input :ticket_price
+      f.input :featured
+      f.input :user_id
+      f.input :date
+      f.input :avatar
+      f.input :flyer
+      f.input :description
+      f.input :latitude
+      f.input :longitude
+      f.input :start
+      f.input :end
+      f.input :venue
+      f.input :url
+    end
+
+  #  controller do 
+  #   def permitted_params
+  #     params.permit event: [ :event_name 
+  #     :location, 
+  #     :borough, 
+  #     :start_time, 
+  #     :end_time, 
+  #     :ticket_price, 
+  #     :ticket_add_1, 
+  #     :ticket_add_2, 
+  #     :featured,
+  #     :approved,
+  #     :start,
+  #     :end ]
+  #   end
+  # end
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # permit_params :list, :of, :attributes, :on, :model
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:permitted, :attributes]
+  #   permitted << :other if resource.something?
+  #   permitted
+  # end
+
+
 end
-
-
-# :event_name => params[:event][:event_name], 
-#       :location => params[:event][:location], 
-#       :borough => params[:event][:borough],
-#       :ticket_price => params[:event][:ticket_price], 
-#       :ticket_add_1 => params[:event][:ticket_add_1], 
-#       :ticket_add_2 => params[:event][:ticket_add_2], 
-#       :featured => params[:event][:featured],
-#       :user_id => params[:event][:user_id],
-#       :date => t,
-#       :approved => params[:event][:approved],
-#       :avatar => params[:event][:avatar],
-#       :flyer => params[:event][:flyer],
-#       :description => params[:event][:description],
-#       :latitude => params[:event][:latitude],
-#       :longitude => params[:event][:longitude],
-#       :venue => params[:event][:venue],
-#       :url => params[:event][:url],
-#       :start => Chronic.parse(params[:event][:start]),
-#       :end => Chronic.parse(params[:event][:end]),
-#       :venue => params[:event][:venue],
-#       :url => params[:event][:url]
