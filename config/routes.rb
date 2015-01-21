52a66ad3e0f914bf2257486c56c202a68c741342
RJ Dabbar <rjdabbar@gmail.com>
2015-01-21 16:53:44 -0500

    more authentication changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :events
-  root 'events#home'
+  root 'events#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
